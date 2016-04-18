4aeb25d930d52cbd4e418a4eac7c20ceba23097d
Amy Hughes <amy.hughes@guardian.co.uk>
2016-04-18 17:45:47 +0100

    Update series_dash_tables.sql
    Update to run on Hive

--- series_dash_tables.sql
+++ series_dash_tables.sql
@@ -1,75 +1,62 @@
-DELETE
-FROM temp_ah.series_content_30_day;
+CREATE TABLE IF NOT EXISTS temp_ah.series_content_30_day (series_url STRING, episode_url STRING, section STRING, publication_date DATE);
+CREATE TABLE IF NOT EXISTS temp_ah.series_visitors_30_day (episode_url STRING, browser_id STRING, visit_date DATE, pageviews DATE);
+CREATE TABLE IF NOT EXISTS temp_ah.series_visitors_by_episode_visit_number (series_url STRING, section STRING, browser_id STRING, episode_visit_number INT);
+CREATE TABLE IF NOT EXISTS temp_ah.series_visitors_by_days_visited (series_url STRING, section STRING, ophan_visitor_id STRING, days_visiting INT);
 
-INSERT INTO temp_ah.series_content_30_day
-SELECT ctl.web_url series_url,
-       path episode_path,
-       cd.section_name section,
-       CAST(web_publication_date AS DATE) publication_date,
-       CAST(web_publication_date AS DATE) DATE
-FROM clean.content cd
-  INNER JOIN clean.tag_to_content tc ON cd.id = tc.content_id
-  INNER JOIN clean.tag ctl ON tc.tag_id = ctl.id
-WHERE ctl.tag_type = 'Series'
-AND   web_publication_date <= CURRENT_DATE
-AND   web_publication_date >= CURRENT_DATE-INTERVAL '30' DAY
-GROUP BY ctl.web_url,
-         path,
-         cd.section_name,
+INSERT OVERWRITE TABLE temp_ah.series_content_30_day
+SELECT series_tag series_url,
+       content_path episode_url,
+       section_name section,
+       CAST(web_publication_date AS DATE) publication_date
+FROM clean.content LATERAL VIEW explode (series_tags) t AS series_tag
+WHERE series_tags IS NOT NULL
+AND   web_publication_date <= to_date (from_unixtime (unix_timestamp ()))
+AND   web_publication_date >= date_add (to_date (from_unixtime (unix_timestamp ())),-30)
+GROUP BY series_tag,
+         content_path,
+         section_name,
          web_publication_date;
 
-DELETE
-FROM temp_ah.series_visitors_30_day;
-
-INSERT INTO temp_ah.series_visitors_30_day
-SELECT pvf.url_path episode_path,
+INSERT OVERWRITE TABLE temp_ah.series_visitors_30_day
+SELECT p.url_path episode_url,
        browser_id,
        CAST(event_timestamp AS DATE) visit_date,
-       COUNT(*) AS pageviews,
-       'test'
-FROM clean.pageviews pvf
-WHERE event_timestamp <= CURRENT_DATE
-AND   event_timestamp >= CURRENT_DATE-interval '30' day
-AND   pvf.date <= CURRENT_DATE
-AND   pvf.date >= CURRENT_DATE-interval '30' day
-GROUP BY 1,
-         2,
-         3;
+       COUNT(*) AS pageviews
+FROM clean.pageviews p
+WHERE event_timestamp <= to_date (from_unixtime (unix_timestamp ()))
+AND   event_timestamp >= date_add (to_date (from_unixtime (unix_timestamp ())),-30)
+AND   p.date <= to_date (from_unixtime (unix_timestamp ()))
+AND   p.date >= date_add (to_date (from_unixtime (unix_timestamp ())),-30)
+GROUP BY p.url_path,
+         browser_id,
+         CAST(event_timestamp AS DATE);
 
-DELETE
-FROM temp_ah.series_visitors_by_episode_visit_number;
-
-INSERT INTO temp_ah.series_visitors_by_episode_visit_number
+INSERT OVERWRITE TABLE temp_ah.series_visitors_by_episode_visit_number
 SELECT series_url,
        section,
        browser_id,
-       COUNT(sc.episode_path) AS episode_visit_number,
-       'test'
-FROM temp_ah.series_content_30_day sc
-  INNER JOIN temp_ah.series_visitors_30_day sv ON sc.episode_path = sv.episode_path
+       COUNT(sc.episode_url) AS episode_visit_number
+FROM temp_ah.series_visitors_30_day sv
+  INNER JOIN temp_ah.series_content_30_day sc ON sc.episode_url = sv.episode_url
 GROUP BY series_url,
          section,
          browser_id;
 
-DELETE
-FROM temp_ah.series_visitors_by_days_visited;
-
-INSERT INTO temp_ah.series_visitors_by_days_visited
+INSERT OVERWRITE TABLE temp_ah.series_visitors_by_days_visited
 SELECT series_url,
        section,
        browser_id,
-       COUNT(visit_date) AS days_visiting,
-       'test'
+       COUNT(visit_date) AS days_visiting
 FROM (SELECT series_url,
              section,
              browser_id,
              visit_date
-      FROM temp_ah.series_content_30_day AS sc
-        INNER JOIN temp_ah.series_visitors_30_day AS sv ON sc.episode_path = sv.episode_path
-      GROUP BY 1,
-               2,
-               3,
-               4)
-GROUP BY 1,
-         2,
-         3
+      FROM temp_ah.series_visitors_30_day AS sv
+        INNER JOIN temp_ah.series_content_30_day AS sc ON sc.episode_url = sv.episode_url
+      GROUP BY series_url,
+               section,
+               browser_id,
+               visit_date) a
+GROUP BY series_url,
+         section,
+         browser_id;
